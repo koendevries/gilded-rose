3a9fb32b41aa2ed03490dc812e8e994da932165f
koendevries <koen.devries@persgroep.net>
2021-08-27 16:05:16 +0200

    add results of TexttestFixture

--- Java/src/test/java/com/gildedrose/TexttestFixtureTest.java
+++ Java/src/test/java/com/gildedrose/TexttestFixtureTest.java
@@ -1,10 +1,13 @@
 package com.gildedrose;
 
-public class TexttestFixture {
-    public static void main(String[] args) {
-        System.out.println("OMGHAI!");
+import org.approvaltests.Approvals;
+import org.junit.jupiter.api.Test;
 
-        Item[] items = new Item[] {
+class TextFixtureTest {
+
+    @Test
+    void testItems() {
+        Item[] items = new Item[]{
                 new Item("+5 Dexterity Vest", 10, 20), //
                 new Item("Aged Brie", 2, 0), //
                 new Item("Elixir of the Mongoose", 5, 7), //
@@ -13,25 +16,15 @@ public class TexttestFixture {
                 new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
-                // this conjured item does not work properly yet
-                new Item("Conjured Mana Cake", 3, 6) };
-
+                new Item("Conjured Mana Cake", 3, 6)};
         GildedRose app = new GildedRose(items);
 
-        int days = 2;
-        if (args.length > 0) {
-            days = Integer.parseInt(args[0]) + 1;
-        }
+        int days = 20;
 
         for (int i = 0; i < days; i++) {
-            System.out.println("-------- day " + i + " --------");
-            System.out.println("name, sellIn, quality");
-            for (Item item : items) {
-                System.out.println(item);
-            }
-            System.out.println();
             app.updateQuality();
         }
+        Approvals.verifyAll("", items);
     }
 
 }
