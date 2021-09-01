b82cb5e253a4e47fc8d08b8c14528f18ac7fc86b
koendevries <koen.devries@persgroep.net>
2021-09-01 17:20:39 +0200

    improve parameterized test

--- Java/src/test/java/com/gildedrose/updatableitems/BackstagePassesQualityDeltaStrategyTest.java
+++ Java/src/test/java/com/gildedrose/updatableitems/BackstagePassesQualityDeltaStrategyTest.java
@@ -1,38 +1,39 @@
 package com.gildedrose.updatableitems;
 
 import com.gildedrose.Item;
-import com.gildedrose.util.ParameterizedTestData;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class BackstagePassesQualityDeltaStrategyTest {
 
     private final QualityDeltaStrategy backstagePassesQualityDeltaStrategy = new BackstagePassesQualityDeltaStrategy();
 
+    static Stream<Arguments> backstagePassesProvider() {
+        return Stream.of(
+                arguments(new Item("foo", 11, 10), 1),
+                arguments(new Item("foo", 10, 10), 2),
+                arguments(new Item("foo", 6, 10), 2),
+                arguments(new Item("foo", 5, 10), 3),
+                arguments(new Item("foo", 0, 10), 3),
+                arguments(new Item("foo", -1, 10), -10),
+                arguments(new Item("foo", -2, 10), 0)
+        );
+    }
+
     @ParameterizedTest
     @MethodSource("backstagePassesProvider")
-    void shouldUpdateBackstageQuality(ParameterizedTestData parameterizedTestData) {
-        final UpdatableItem updatableItem = UpdatableItemFactory.create(parameterizedTestData.item);
+    void shouldUpdateBackstageQuality(Item item, int expected) {
+        final UpdatableItem updatableItem = UpdatableItemFactory.create(item);
 
         final int delta = backstagePassesQualityDeltaStrategy.qualityDelta(updatableItem);
 
-        assertEquals(parameterizedTestData.expected, delta);
-    }
-
-    static Stream<ParameterizedTestData> backstagePassesProvider() {
-        return Stream.of(
-                new ParameterizedTestData(new Item("foo", 11, 10), 1),
-                new ParameterizedTestData(new Item("foo", 10, 10), 2),
-                new ParameterizedTestData(new Item("foo", 6, 10), 2),
-                new ParameterizedTestData(new Item("foo", 5, 10), 3),
-                new ParameterizedTestData(new Item("foo", 0, 10), 3),
-                new ParameterizedTestData(new Item("foo", -1, 10), -10),
-                new ParameterizedTestData(new Item("foo", -2, 10), 0)
-        );
+        assertEquals(expected, delta);
     }
 
 }
